973feffc54596731e1df6ee012aa699d667b8147
ionescu vlad <ionescu.vlad1@gmail.com>
2020-05-16 09:21:39 +0300

    Reorganize folders and add Flower model.

--- FlowersApp/Controllers/WeatherForecastController.cs
+++ FlowersApp/Controllers/WeatherForecastController.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using FlowersApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+
 
 namespace FlowersApp.Controllers
 {
---
+++ FlowersApp/Models/Flower.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlowersApp.Models
+{
+    public enum FlowerUpkeepDifficulty
+    {
+        Easy,
+        Medium,
+        Hard
+    }
+
+    public class Flower
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTimeOffset DateAdded { get; set; }
+        public long MarketPrice { get; set; }
+        public FlowerUpkeepDifficulty FlowerUpkeepDifficulty { get; set; }
+    }
+}
