dbcf0539a079fa8086514672c3cb33467b31b736
Alexandru Oancea <alx.oancea@gmail.com>
2020-05-22 23:06:32 +0300

    add post for flowers

--- FlowersApp/ClientApp/src/app/home/home.component.ts
+++ FlowersApp/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
+    selector: 'app-home',
+    templateUrl: './home.component.html',
 })
 export class HomeComponent {
+
+    name: string = "test"
+
+
+    constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
+    }
+
+    submit() {
+
+        var flower: Flower = <Flower>{};
+        flower.name = this.name;
+        flower.description = this.name;
+        flower.dateAdded = new Date();
+        flower.flowerUpkeepDifficulty = FlowerUpkeepDifficulty.Easy;
+        flower.marketPrice = 5;
+
+        this.http.post(this.baseUrl + 'api/Flowers', flower).subscribe(result => {
+
+            console.log('success!');
+
+        },
+            error => {
+
+                if (error.status == 400) {
+                    console.log(error.error.errors)
+                }
+            });
+    }
+}
+
+interface Flower {
+    dateAdded: Date;
+    name: string;
+    description: string;
+    marketPrice: number;
+    flowerUpkeepDifficulty: FlowerUpkeepDifficulty;
+}
+
+enum FlowerUpkeepDifficulty {
+    Easy = 0,
+    Medium = 1,
+    Hard = 2
 }
